fa8bcd5fde8d1ac449d48bb7b4558f8e0190f6de
Leklef <lenar8553@gmail.com>
2017-03-22 15:55:15 +0300

    Init

---
+++ Pagination/DataModel.swift
@@ -0,0 +1,19 @@
+//
+//  DataModel.swift
+//  Pagination
+//
+//  Created by Ленар on 22.03.17.
+//  Copyright © 2017 com.lenar. All rights reserved.
+//
+
+import Foundation
+
+class DataForTable {
+    var date:Date;
+    var text:String;
+    
+    init() {
+        self.date = Date()
+        self.text = "LoL"
+    }
+}
